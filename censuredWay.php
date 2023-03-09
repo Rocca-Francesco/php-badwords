b23a89e0f7a657d7e046123b5219d8d4aea97e3d
Rocca-Francesco <roccafrance98@yahoo.it>
2023-03-09 15:41:58 +0100

    visualizzazione a schermo con html

--- censuredWay.php
+++ censuredWay.php
@@ -1,11 +1,8 @@
 <?php
 $yourText = $_GET["yourText"];
-var_dump($yourText);
 $censuredWord = $_GET["censuredWord"];
-var_dump($censuredWord);
 
 $censuredText = str_replace($censuredWord,"***",$yourText);
-var_dump($censuredText);
 ?>
 
 
@@ -17,8 +14,17 @@ var_dump($censuredText);
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Testo censurato</title>
+  
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-  
+  <div class="container">
+    <h2>Il testo originale era:</h2>
+    <p><?= $yourText ?></p>
+    <h2>Il testo censurato è:</h2>
+    <p><?= $censuredText ?></p>
+  </div>
 </body>
 </html>
